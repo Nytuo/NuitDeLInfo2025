f538180558f3dd56ef414021246e604f3502ea32
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2025-12-05 03:21:25 +0100

    doc

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/NuitDeLInfo2025/frontend/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
